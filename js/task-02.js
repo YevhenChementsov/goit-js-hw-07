746465d587b14ae88bd906a97a8d52130fc8e845
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-07-01 21:06:06 +0300

    Добавляет задание-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+console.log(ingredientsListRef);
+
+const allIngredients = ingredients.map((ingredient) => {
+  const ingredientsListItemRef = document.createElement('li');
+  ingredientsListItemRef.textContent = ingredient;
+  return ingredientsListItemRef;
+});
+
+ingredientsListRef.append(...allIngredients);
