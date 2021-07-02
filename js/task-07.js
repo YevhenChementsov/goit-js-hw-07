dab818f63e9779df64ceb8e5fda3e7a9d1c9dce8
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-07-03 01:08:03 +0300

    Добавляет задание-07

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const fontSizeControlRef = document.querySelector('#font-size-control');
+const inputTextRef = document.querySelector('#text');
+
+const inputFontSizeChangeRef = () => inputTextRef.style.fontSize = fontSizeControlRef.value + 'px';
+
+fontSizeControlRef.addEventListener('input', inputFontSizeChangeRef);
